45b080031745e54e3abc8a01d23ecc9a060c5f5d
Qwerrty574 <ilyinsky2001@gmail.com>
2023-03-02 01:59:38 +0300

    add instrumentator

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI, Depends
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
@@ -8,6 +14,22 @@ from models import Base, Document
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+resource = Resource(attributes={
+    SERVICE_NAME: "documents-service"
+})
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831,
+)
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(jaeger_exporter)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+FastAPIInstrumentor.instrument_app(app)
 
 
 def get_db():
