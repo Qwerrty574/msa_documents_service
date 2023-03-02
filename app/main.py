436bca797efd7929c68cb325b82e2d195dba127f
Qwerrty574 <ilyinsky2001@gmail.com>
2023-03-02 16:37:37 +0300

    fix some errors

--- app/main.py
+++ app/main.py
@@ -63,9 +63,9 @@ async def create_document(doc: DocumentObj, db: Session = Depends(get_db)):
 @app.get("/documents/{document_id}")
 async def read_document(document_id: int, db: Session = Depends(get_db)):
     document = db.query(Document).filter(Document.id == document_id).first()
-    if not document:
-        raise HTTPException(status_code=404, detail="Document not found")
-    return document
+    if document:
+        return document
+    raise HTTPException(status_code=404, detail="Document not found")
 
 
 @app.get("/documents/")
